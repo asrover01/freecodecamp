7a0dfceed62fb03ef932e2a31d16a77b15a6826a
asrover01 <asrover@hotmail.com>
2025-03-22 14:36:59 +0000

    Update script.js

--- javaScript/palindrome/script.js
+++ javaScript/palindrome/script.js
@@ -1,15 +1,16 @@
 const checkButton = document.getElementById('check-btn');
 let resultText = document.getElementById('result');
-
+const checkText = document.getElementById('text-input')
 
 function palindromeCheck(){
-  const checkText = document.getElementById('text-input').value;
+  const inputText = checkText.value;
+
   if (checkText === "") {
   alert("Please input a value");
   return;
 }
 
-  const cleanText= checkText.toLowerCase().replace(/[^a-z0-9]/g,"");
+  const cleanText= inputText.toLowerCase().replace(/[^a-z0-9]/g,"");
   const reverseText = cleanText.split('').reverse().join('');
   if (cleanText === reverseText){
   resultText.innerText = `${checkText} is a palindrome`;} 
