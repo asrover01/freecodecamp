3455cd2b031f30b55598203b7ea4e473dc3ddab1
asrover01 <190014000+asrover01@users.noreply.github.com>
2026-08-16 10:57:46 +0100

    Update script.js

--- javaScript/07-debugging/script.js
+++ javaScript/07-debugging/script.js
@@ -1,4 +1,5 @@
-const darkColorsArr = [
+//old code to choose between a set of colors
+/*const darkColorsArr = [
     "#2C3E50",
     "#34495E",
     "#2C2C2C",
@@ -14,15 +15,43 @@ const darkColorsArr = [
   function getRandomIndex() {
     const randomIndex = Math.floor(darkColorsArr.length * Math.random());
     return randomIndex;
-  }
-  console.log(getRandomIndex())
+  }*/
+//new code to generate random hex color
+    const getRandomIndex = () =>{
+      let color = "";
+      for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * 16);
+        color += randomIndex.toString(16);
+        switch (randomIndex.toString(16)) {
+          case "10":
+            color += "A";
+            break;
+          case "11":
+            color += "B"; 
+            break;
+          case "12":
+            color += "C";
+            break;
+          case "13":
+            color += "D";
+            break;
+          case "14":
+            color += "E";
+            break;
+          case "15":
+            color += "F";
+            break;  
+        }
+      }
+      return "#" + color;
+    }
   
   const body = document.querySelector("body");
   const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
   console.log(bgHexCodeSpanElement)
   
   function changeBackgroundColor() {
-    const color = darkColorsArr[getRandomIndex()];
+    const color = getRandomIndex();
   
     bgHexCodeSpanElement.innerText = color;
     body.style.backgroundColor = color;
